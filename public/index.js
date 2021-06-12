e11d8ac9b12ddb69d0693ed010500028244a245b
Gene Phelps <gpphelpsii@gmail.com>
2021-06-12 09:41:23 -0400

    added in service worker registration

--- public/index.js
+++ public/index.js
@@ -24,6 +24,15 @@ function populateTotal() {
   totalEl.textContent = total;
 }
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then((reg) => {
+        console.log('Service worker registered.', reg);
+      });
+  });
+}
+
 function populateTable() {
   let tbody = document.querySelector("#tbody");
   tbody.innerHTML = "";
