0866438de9e3a911bc0c50e51cdd3958e03fad16
Gene Phelps <gpphelpsii@gmail.com>
2021-06-12 22:36:38 -0400

    added missing s to styles.css in my files to cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,12 +1,14 @@
 const FILES_TO_CACHE = [
     "/",
     "/index.html",
-    "/style.css",
+    "/styles.css",
     "/index.js",
     "/icons/icon-192x192.png",
     "/icons/icon-512x512.png",
     '/service-worker.js',
     "/db.js",
+    'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
+    'https://cdn.jsdelivr.net/npm/chart.js@2.8.0'
 ];
 
 const CACHE_NAME = "static-budget-cache";
