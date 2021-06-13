575dc1af265e221b5d4ea02d2e6f8ee5d3efedc1
Gene Phelps <gpphelpsii@gmail.com>
2021-06-12 22:23:16 -0400

    added / to files to cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -5,7 +5,7 @@ const FILES_TO_CACHE = [
     "/index.js",
     "/icons/icon-192x192.png",
     "/icons/icon-512x512.png",
-    'service-worker.js',
+    '/service-worker.js',
     "/db.js",
     'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
     'https://cdn.jsdelivr.net/npm/chart.js@2.8.0'
